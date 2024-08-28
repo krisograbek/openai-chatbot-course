f08daeaaed35cb5a68c23f264a2d0af573015173
krisograbek <krzysztof.ograbek@gmail.com>
2024-08-28 12:01:07 +0200

    add streaming

--- chatbot.py
+++ chatbot.py
@@ -9,38 +9,38 @@ client = OpenAI()
 
 st.title("My GPT-4o Mini Chatbot 🤖")
 
-# step 1 keep messages
+# Initialize messages in the session state
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Step 1 show messages
+# Display messages
 for message in st.session_state["messages"]:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if "model" not in st.session_state:
-    st.session_state.model = "gpt-4o-mini"
-
-if user_prompt := st.chat_input("Your prompt"):
-    # step 1 Save user message
+if user_prompt := st.chat_input("Your Prompt:"):
     st.session_state.messages.append({"role": "user", "content": user_prompt})
-
-    # step 0: display user prompt
     with st.chat_message("user"):
         st.markdown(user_prompt)
 
-    # generate responses
     with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        completion = client.chat.completions.create(
-            model=st.session_state.model,
-            # step 2: pass the entire
+        chatbot_msg = st.empty()
+        full_response = ""
+        stream = client.chat.completions.create(
+            model="gpt-4o-mini",
             messages=[
                 {"role": msg["role"], "content": msg["content"]}
-                for msg in st.session_state.messages
+                for msg in st.session_state["messages"]
             ],
+            stream=True,
         )
-        response = completion.choices[0].message.content
-        message_placeholder.markdown(response)
 
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        for chunk in stream:
+            token = chunk.choices[0].delta.content
+            if token is not None:
+                full_response = full_response + token
+                chatbot_msg.markdown(full_response)
+
+        chatbot_msg.markdown(full_response)
+
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
